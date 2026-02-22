a017ff35c9da21406158afb8a8bb7d19913c3e3e
unknown <manojchoppa7@gmail.com>
2026-02-22 11:40:21 +0530

    Fix CORS and update frontend routing

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: [
+    "http://localhost:5173",
+    process.env.CLIENT_URL
+  ],
   credentials: true
 }));
-
 app.use(express.json());
 app.use("/api/match", matchRoutes);
 // 🔥 ADD THIS
