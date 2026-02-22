9ebd5118f5c32a5156589a419b20511f83ab8a8c
unknown <manojchoppa7@gmail.com>
2026-02-22 08:22:35 +0530

    Full stack AI Job Tracker

---
+++ client/src/App.jsx
@@ -0,0 +1,69 @@
+import { Routes, Route } from "react-router-dom";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Jobs from "./pages/Jobs";
+import Resume from "./pages/Resume";
+import Analyzer from "./pages/Analyzer";
+import Matcher from "./pages/Matcher";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <Routes>
+      {/* Public */}
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+
+      {/* Protected with Layout */}
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Layout>
+              <Dashboard />
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/jobs"
+        element={
+          <ProtectedRoute>
+            <Layout>
+              <Jobs />
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/resume"
+        element={
+          <ProtectedRoute>
+            <Layout>
+              <Resume />
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/analyzer"
+        element={
+          <ProtectedRoute>
+            <Layout>
+              <Analyzer />
+            </Layout>
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/matcher" element={<Matcher />} />
+    </Routes>
+  );
+}
+
+export default App;
